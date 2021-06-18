41a5f45bd7895bfab7e11b1d5d5d74b577a629e6
Gabrielle Arruda <gabriellearrudac@gmail.com>
2021-06-18 17:44:49 -0300

    apresentacao app

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+# import plotly.express as px
 
 dados = pd.read_csv('dados/9deJulho_reduzido.csv')
-ocupacao = st.selectbox("Selecione a Ocupação: ",["Todas", list(dados["Ocupacao"]),)
-if ocupacao != "Todas":
-    dados = dados[dados.Ocupacao == ocupacao]
+ocupacao = st.selectbox("Selecione a Ocupação: ", ["Todas"] + list(dados["Ocupacao"].unique()))
+st.dataframe(dados)
 # import pdb; pdb.set_trace()
-x = st.slider('Select a value')
-st.write(x, 'squared is', x * x)
+
+
+# session_state.escola = st.selectbox(
+#     "Selecione a Ocupação: ",
+#     ["", "Minha escola não está na lista"] + list(data["codinep_nomedaescola"]),
+# )
+# if ocupacao != "Todas":
+#     dados = dados[dados.Ocupacao == ocupacao]
+# x = st.slider('Select a value')
+# st.write(x, 'squared is', x * x)
