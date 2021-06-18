a59b039fa977a934644cabf5f8f1c6af2a0e93fb
Gabrielle Arruda <gabriellearrudac@gmail.com>
2021-06-18 17:07:40 -0300

    add streamlit app

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
+dados = pd.read_csv('dados/9deJulho_reduzido.csv')
+ocupacao = st.selectbox("Selecione a Ocupação: ",["Todas", list(dados["Ocupacao"]),)
+if ocupacao != "Todas":
+    dados = dados[dados.Ocupacao == ocupacao]
+# import pdb; pdb.set_trace()
 x = st.slider('Select a value')
 st.write(x, 'squared is', x * x)
